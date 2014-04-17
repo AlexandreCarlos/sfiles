74a8301f364f4ecdcff95061fedf9f8bdd90363a
Alexandre Carlos <alexandre.carlos@gmail.com>
2014-04-17 11:33:24 +0100

    Scanning and giving file sizes

--- ListDir.py
+++ ListDir.py
@@ -2,15 +2,16 @@
 # -*- coding: utf-8 -*-
 
 import os
+from os.path import join, getsize
 
-for dirname, dirnames, filenames in os.walk('.'):
+for dirname, dirnames, filenames in os.walk('C:\\Users\\KKU035\\Documents\\Dropbox\\Python_Books', topdown=True):
     # print path to all subdirectories first.
     for subdirname in dirnames:
-        print os.path.join(dirname, subdirname)
+        print join(dirname, subdirname)
 
     # print path to all filenames.
     for filename in filenames:
-        print os.path.join(dirname, filename)
+        print join(dirname, filename), getsize(join(dirname, filename)) 
 
     # Advanced usage:
     # editing the 'dirnames' list will stop os.walk()
