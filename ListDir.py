e69f5a3e1c92530bbf9d94abe9c12453c64b4a63
Alexandre Carlos <alexandre.carlos@gmail.com>
2014-04-18 00:35:52 +0100

    file space on disk

--- ListDir.py
+++ ListDir.py
@@ -5,15 +5,41 @@ from __future__ import (absolute_import,
                         print_function,
                         unicode_literals)
 import os
+import ctypes
 from os.path import join, getsize
+from math import ceil
+
 import dataset
 
+class DiskUsage(object):
+    def __init__(self):
+        self._sectorsPerCluster = ctypes.c_ulonglong(0)
+        self._bytesPerSector = ctypes.c_ulonglong(0)
+        self._rootPathName = ctypes.c_wchar_p(u"C:\\")
+
+        self._r = ctypes.windll.kernel32.GetDiskFreeSpaceW(self._rootPathName,
+                ctypes.pointer(self._sectorsPerCluster),
+                ctypes.pointer(self._bytesPerSector),
+                None,
+                None,
+                )
+
+        print (self._r)
+
+        assert self._r == 1
+
+    def disk_file_size(self, file_size):
+        r1 = ceil(float(file_size)/float(self._bytesPerSector.value))  # how many sectors
+        r2 = ceil(r1/float(self._sectorsPerCluster.value))
+
+        return long(r2*self._bytesPerSector.value*self._sectorsPerCluster.value)
 
 class Ficheiro(object):
     def __init__(self):
         self._path = ""
         self._filename = ""
-        self._size = ""
+        self._size = 0L
+        self._disk_size = 0L
 
         #connect to the database
         self.db = dataset.connect('sqlite:///file_space.db')
@@ -45,10 +71,19 @@ class Ficheiro(object):
     def size(self, value):
         self._size = value
 
+    @property
+    def disk_size(self):
+        return self._disk_size
+
+    @disk_size.setter
+    def disk_size(self, value):
+        self._disk_size = value
+
     def insert(self):
         self.table.insert(dict(path=self._path,
-                          filename=self._filename,
-                          size=self._size))
+                               filename=self._filename,
+                               size=self._size,
+                               disk_size=self._disk_size))
 
     def delete(self, **_filter):
         self.table.delete(**_filter)
@@ -62,6 +97,7 @@ class Ficheiro(object):
     def rollback(self):
         self.db.rollback()
 
+du = DiskUsage()
 fich = Ficheiro()
 
 # fich.begin()
@@ -69,11 +105,8 @@ fich = Ficheiro()
 # fich.commit()
 fich.begin()
 
-
-for dirname, dirnames, filenames in os.walk('C:\\Users\\'
-                                            + 'KKU035\\Documents\\'
-                                            + 'Dropbox\\Python_Books',
-                                            topdown=True):
+for dirname, dirnames, filenames in os.walk('C:\\Users\\Alexandre\\Documents\\Dropbox\\Python_Books',
+topdown=True):
     # print path to all subdirectories first.
     # for subdirname in dirnames:
     #     print join(dirname, subdirname)
@@ -83,9 +116,10 @@ for dirname, dirnames, filenames in os.walk('C:\\Users\\'
         fich.path = dirname
         fich.filename = filename
         fich.size = getsize(join(dirname, filename))
+        fich.disk_size = du.disk_file_size(fich.size)
         fich.insert()
-        print (join(dirname, filename).encode('utf-8'),
-               getsize(join(dirname, filename)))
+        print(join(dirname, filename).encode('utf-8'),
+              getsize(join(dirname, filename)), fich.disk_size)
 
     # Advanced usage:
     # editing the 'dirnames' list will stop os.walk()
